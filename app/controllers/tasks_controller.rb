8fb76d0548c6978783abbc107455e3bd557ca0b2
Debbie Pao <dpao@berkeley.edu>
2018-01-26 16:01:42 -0800

    can delete tasks now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,4 +13,10 @@ class TasksController < ApplicationController
   def show
      @task = Task.find(params[:id])
   end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
 end
